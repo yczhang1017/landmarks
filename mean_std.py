67e1ad5455c06569cfcdf8053a06d0861600d0c5
yczhang1017 <zhangyuchen12344@gmail.com>
2019-04-17 01:42:26 -0400

    Add files via upload

--- mean_std.py
+++ mean_std.py
@@ -21,8 +21,8 @@ for i,ind in enumerate(randl):
     id=df.at[ind,'id']
     image_path=os.path.join(image_dir,id[0],id[1],id[2],id+'.jpg')
     img3=imread(image_path)
-    std=np.std(np.std(img3, axis=0),axis=0)
-    mean=np.mean(np.std(img3, axis=0),axis=0)
+    std=np.std(img3,axis=(0,1))
+    mean=np.mean(img3,axis=(0,1))
     all_mean+=mean
     all_std+=std
 
