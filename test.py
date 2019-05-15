c6165fdb9cebc61271a7f8011c3e139d66fd4756
yczhang1017 <zhangyuchen12344@gmail.com>
2019-05-15 00:31:10 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -201,7 +201,7 @@ def main():
                         results.append(label2id[label])
                         ii=ii+1
                     
-                    score=subscore[:,0]*subscore[:,1]
+                    score=subscore[:,0]*subscore[:,1]*100
                     confidence=confidence+score.tolist()
                         
                         
@@ -209,17 +209,17 @@ def main():
             t02= time.time()
             dt1=(t02-t01)
             if (ib+1)%10==0:
-                print('Image {:d}/{:d} time: {:.6f}s'.format(ii,total,dt1))
+                print('Image {:d}/{:d} time: {:.4f}s'.format(ii,total,dt1))
     
     
     results=results[:len(image_ids)]
     confidence=confidence[:len(image_ids)]
     print('number of detected labels: ',len(results))
-    df.loc[image_ids,'landmarks']=['{:d} {:.4f}'.format(r,c) for r,c in zip(results,confidence)]
+    df.loc[image_ids,'landmarks']=['{:d} {:.6e}'.format(r,c) for r,c in zip(results,confidence)]
     detected = pd.DataFrame({'landmarks':results}, index =image_ids) 
     most=detected.groupby('landmarks').size().idxmax()
     
-    fornone='{:d} 0.001'.format(most)
+    fornone='{:d} 1e-4'.format(most)
     print('nones are asigned: ',fornone)
     df.loc[nones,'landmarks']=fornone
     df.to_csv(path_or_buf='results.csv')
