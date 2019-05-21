902987f69e1d45353af1ab075d56a57183622d43
yczhang1017 <zhangyuchen12344@gmail.com>
2019-05-21 12:55:58 -0400

    Add files via upload

--- snet.py
+++ snet.py
@@ -355,7 +355,6 @@ class SNet(nn.Module):
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
-        x = self.layer4(x)
         y=[None]*2
         for i,c in enumerate(self.num_classes): 
             y[i] = self.layer4[i](x)
