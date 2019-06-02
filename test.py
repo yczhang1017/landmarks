2c77e2161e3874dc480a9a1c997e78cab16a6cfc
yczhang1017 <zhangyuchen12344@gmail.com>
2019-06-01 17:30:33 -0700

    Add files via upload

--- test.py
+++ test.py
@@ -8,8 +8,9 @@ import torch.utils.data
 import torch.utils.data.distributed
 import time
 import argparse
-import rnet
-from rnet import NLABEL,PRIMES,mean,std
+#import rnet
+import snet as mynet
+from train import NLABEL,PRIMES,mean,std
 
 import torchvision.models as models
 try:
@@ -31,12 +32,11 @@ parser = argparse.ArgumentParser(
     description='Google Landmarks Recognition')
 parser.add_argument('--data', metavar='DIR',default='./test',
                     help='path to dataset')
-
-parser.add_argument('-a', '--arch', metavar='ARCH', default='rnet34',
+parser.add_argument('-a', '--arch', metavar='ARCH', default='s_resnext50_32x4d',
                     choices=model_names,
                     help='model architecture: ' +
                         ' | '.join(model_names) +
-                        ' (default: resnet18)')
+                        ' (default: s_resnext50_32x4d)')
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
 parser.add_argument('-b', '--batch_size', default=512, type=int,
@@ -48,6 +48,12 @@ parser.add_argument('-s','--save_folder', default='save/', type=str,
                     help='Dir to save results')
 
 
+parser.add_argument('-s1', '--val_size', default=224, type=int,
+                    metavar='N', help= 'image size for decoding')
+parser.add_argument('-s2', '--crop_size', default=224, type=int,
+                    metavar='N', help = 'image size for crop')
+                    
+        
 parser.add_argument('-p', '--print-freq', default=10, type=int,
                     metavar='N', help='print frequency (default: 10)')
 parser.add_argument('--dali_cpu', action='store_true',
@@ -83,15 +89,17 @@ def main():
     txt_path=os.path.join(args.data,'file_list.txt')
     file1 = open(txt_path,"w")
     image_ids=[]
-
-    for jpg in os.listdir(args.data):
-        if jpg.endswith('.jpg'):
-            file1.write(jpg+' 0\n')
-            image_ids.append(jpg.split('.')[0])
+    
+    df=pd.read_csv('test.csv',index_col=0)
+    ids=df.index.tolist()
+    
+    for id in ids:
+        file1.write(id[0]+'/'+id[1]+'/'+id[2]+'/'+id+'.jpg 0\n')
+        image_ids.append(id)
     file1.close()
     
-    crop_size = 224
-    val_size = 224
+    crop_size = args.crop_size
+    val_size = args.val_size
     
     
     pipe = HybridValPipe(batch_size=args.batch_size, num_threads=args.workers, device_id=args.local_rank, 
@@ -119,8 +127,8 @@ def main():
                 if args.fp16:
                     model[i] = network_to_half(model[i])
             model[i].eval()
-    elif args.arch in rnet.__dict__:
-        model=rnet.__dict__[args.arch](pretrained=False,num_classes=PRIMES)
+    else:
+        model=mynet.__dict__[args.arch](pretrained=None,num_classes=PRIMES)
         model.load_state_dict(torch.load(args.checkpoint,
                 map_location=lambda storage, loc: storage)['state'])
         if torch.cuda.is_available():
@@ -130,20 +138,22 @@ def main():
     f1=open("label_id.pkl","rb")
     label2id=pickle.load(f1)
     maxlabel=sorted(label2id.keys())[-1]
-    
+    assert maxlabel+1 == NLABEL
     f1.close()
     
-    
+    '''
     sample_path='recognition_sample_submission.csv'
     df=pd.read_csv(sample_path,index_col=0)
     nones=df.index.drop(image_ids)
+    '''
     
     p0=PRIMES[0]
     p1=PRIMES[1]
-    results=[]
-    confidence=[]
+
     dp=p1-p0
     res=[]
+    of=open("results.csv",mode='w+')
+    of.write('id,landmarks\n')
     for j in range(dp):
         res.append((-p0*j)%dp)
         
@@ -157,72 +167,36 @@ def main():
     with torch.no_grad():
         for ib, data in enumerate(dataloader):
             inputs = data[0]["data"].to(device, non_blocking=True)
-            sublabel=torch.zeros((inputs.size(0),len(PRIMES)),dtype=torch.int64)
-            subscore=torch.zeros((inputs.size(0),len(PRIMES)),dtype=torch.float)
-            
-            preds=torch.zeros((inputs.size(0)),dtype=torch.int64).cpu()
-            score=torch.zeros((inputs.size(0)),dtype=torch.float).cpu()
-            count=inputs.shape[0]
+            nn=inputs.shape[0]
             
             softmax=torch.nn.Softmax(dim=1)
             if args.arch in model_names:
-                for i,p in enumerate(PRIMES):
-                    outputs=model[i](inputs)
-                    outputs=softmax(outputs)
-                    subscore[:,i],sublabel[:,i] = outputs.max(dim=1)
-                    
-            elif args.arch in rnet.__dict__:
+                outputs=model[0](inputs)
+                scores=softmax(outputs).unsqueeze(2).expand((nn,p0,p1))
+                
+                outputs=model[1](inputs)
+                scores= scores * softmax(outputs).unsqueeze(1).expand((nn,p0,p1))
+                
+            else:
                 outputs=model(inputs)
-                for i,p in enumerate(PRIMES):
-                    outputs[i]=softmax(outputs[i])
-                    subscore[:,i],sublabel[:,i] = outputs[i].max(dim=1)
-                
-                
-            for i,p in enumerate(PRIMES):
-                if i>0:
-                    preds=((sublabel[:,0]-sublabel[:,i])%p0).cpu()
-                    preds.apply_(tolabel)
-                    preds=preds+sublabel[:,i]
-                    
-                    for j in range(count):
-                        label=preds[j].item()
-                        if label > maxlabel:
-                            if args.arch in model_names:
-                                scoj,pros =outputs[j,:].topk(20)
-                            elif args.arch in rnet.__dict__:
-                                scoj,pros =outputs[1][j,:].topk(20)
-                            pros=pros.cpu()
-                            k=0
-                            while label > maxlabel:
-                                k=k+1
-                                preds_j=(sublabel[j,0]-pros[k])%p0
-                                label=tolabel(preds_j.item())+pros[k].item()
-                            subscore[j,1]=scoj[k]
-                        results.append(label2id[label])
+                scores=softmax(outputs[0]).unsqueeze(2).expand((nn,p0,p1))
+                scores= scores * softmax(outputs[1]).unsqueeze(1).expand((nn,p0,p1))
+                scores= scores.reshape((scores.shape[0],p0*p1))
+                scores[:,NLABEL:]=0
+                scores = scores/scores.sum(dim=1,keepdim=True)
+                conf, pred = scores.max(dim=1)
+                for j in range(nn):
+                    if ii< len(ids):
+                        of.write('{:s},{:d} {:.6f}\n'.format(ids[ii].split('.')[0],
+                                 label2id[pred[j].item()],conf[j].item()))
                         ii=ii+1
-                    
-                    score=subscore[:,0]*subscore[:,1]
-                    confidence=confidence+score.tolist()
-                        
                         
             t01= t02
             t02= time.time()
             dt1=(t02-t01)
             if (ib+1)%10==0:
                 print('Image {:d}/{:d} time: {:.4f}s'.format(ii,total,dt1))
-    
-    
-    results=results[:len(image_ids)]
-    confidence=confidence[:len(image_ids)]
-    print('number of detected labels: ',len(results))
-    df.loc[image_ids,'landmarks']=['{:d} {:.6f}'.format(r,c) for r,c in zip(results,confidence)]
-    detected = pd.DataFrame({'landmarks':results}, index =image_ids) 
-    most=detected.groupby('landmarks').size().idxmax()
-    
-    fornone='{:d} 0.0001'.format(most)
-    print('nones are asigned: ',fornone)
-    df.loc[nones,'landmarks']=fornone
-    df.to_csv(path_or_buf='results.csv')
+    of.close()
                 
 if __name__ == '__main__':
     main()   
